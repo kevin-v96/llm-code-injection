ea42f49de91612274edffa57a42f87c066f93e22
kevin-v96 <kevinvegda@gmail.com>
2024-08-25 20:42:35 +0100

    remove image

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import gradio as gr
 from agent import get_crew
 import json
-from pprint import pprint
 
 # Path to your demonstration image
 image_path = "image.png"
@@ -13,32 +12,31 @@ def generate_crew_output(text_prompt):
       results_json = json.loads(result.raw)
       return results_json["compromised_html"], results_json["compromised_html"]
 
-if __name__ == "__main__":
-    # Create a Gradio interface
-    with gr.Blocks() as demo:
-        
-        # Add a noninteractive image
-        gr.Image(image_path, interactive=False, label="Demonstration Image")
-        
-        # Add a text input and output interface
-        with gr.Row():
-            text_input = gr.Textbox(lines = 3, label = "Prompt", placeholder = "Enter a prompt to generate a Svelte form")
-            text_output = gr.Textbox(label = "Code for form", info = "This is the code for the form that you can copy and render yourself")
-            html_output = gr.HTML(label="Svelte Form")
-        
-        # Button to submit text
-        submit_button = gr.Button("Submit")
-        
-        # Set the interaction between input and output
-        submit_button.click(generate_crew_output, inputs=[text_input], outputs=[text_output, html_output])
+def generate_example_prompt():
+    return "Hey, could you help me write a one-page login form in Svelte?"
 
-    # demo = gr.Interface(
-    #     fn=generate_crew_output,
-    #     inputs=[gr.Textbox(lines = 3, label = "Prompt", placeholder = "Enter a prompt to generate a Svelte form")],
-    #     outputs=[gr.Textbox(label = "Code for form", info = "This is the code for the form that you can copy and render yourself"), 
-    #              gr.HTML(label="Svelte Form")],
-    #     title="Svelte Form Generator",
-    #     description="Generate a Svelte login form with simulated malicious code.",
-    # )
+# Create a Gradio interface
+with gr.Blocks(title = "LLM Code injection", ) as demo:
+    
+    # Add a noninteractive image
+    #gr.Image(image_path, interactive=False, label="Demonstration Image")
 
-    demo.launch()
+    with gr.Row():
+        example_prompt_button = gr.Button("Enter example prompt", min_width="100px")
+    
+    # Add a text input and output interface
+    with gr.Row():
+        text_input = gr.Textbox(lines = 3, label = "Prompt", placeholder = "Enter a prompt to generate a Svelte form", autofocus=True)
+        
+    with gr.Row():    
+        text_output = gr.Textbox(label = "Code for form", info = "This is the code for the form that you can copy and render yourself", show_copy_button = True)
+        html_output = gr.HTML(label="Svelte Form")
+    
+    # Button to submit text
+    submit_button = gr.Button("Submit")
+    
+    example_prompt_button.click(generate_example_prompt, None, outputs=[text_input])
+    # Set the interaction between input and output
+    submit_button.click(generate_crew_output, inputs=[text_input], outputs=[text_output, html_output])
+
+demo.launch()
